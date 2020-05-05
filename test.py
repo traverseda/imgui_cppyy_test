49c0fc3852707f612377f5117a9ec7907427186d
Alex Davies <traverse.da@gmail.com>
2020-05-05 14:02:23 -0300

    Basic test

---
+++ test.py
@@ -0,0 +1,6 @@
+import imgui
+imgui.ImGui.CreateContext()
+
+#As per https://github.com/ocornut/imgui/blob/8bcac7d95caa7e3b960b7bf7c3bcda4ecfe4541f/examples/imgui_impl_glfw.cpp#L150
+io = imgui.ImGui.GetIO()
+io.BackendPlatformName = "imgui_impl_test" #This should be setable
