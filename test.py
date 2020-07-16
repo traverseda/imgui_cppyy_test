480845227ed32b5e399905ea14d262d6c772cb60
Alex Davies <traverse.da@gmail.com>
2020-07-16 11:59:24 -0300

    It seems to mostly work...

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ imgui.ImGui.CreateContext()
 #As per https://github.com/ocornut/imgui/blob/8bcac7d95caa7e3b960b7bf7c3bcda4ecfe4541f/examples/imgui_impl_glfw.cpp#L150
 io = imgui.ImGui.GetIO()
 io.BackendPlatformName = "imgui_impl_test" #This should be setable
+print(io.BackendPlatformName)
